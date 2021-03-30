0165f4426ff431a41d989953c698926522350b68
Tolunay Kandırmaz <kandirmaz16@itu.edu.tr>
2021-03-30 22:51:30 +0300

    Onboarding screen created and some fragments added

--- app/src/main/java/com/tolunaykandirmaz/signverify/MainActivity.java
+++ app/src/main/java/com/tolunaykandirmaz/signverify/MainActivity.java
@@ -1,14 +1,52 @@
 package com.tolunaykandirmaz.signverify;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.appintro.AppIntro;
+import com.github.appintro.AppIntroFragment;
+import com.github.appintro.AppIntroPageTransformerType;
+import com.tolunaykandirmaz.signverify.onboarding.fragment.CustomOnboardingFragment1;
+
+public class MainActivity extends AppIntro {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        addSlide(AppIntroFragment.newInstance(getString(R.string.hosgeldin), getString(R.string.fragement1_tanitim_yazisi), R.mipmap.ic_launcher, Color.BLUE));
+
+        addSlide(CustomOnboardingFragment1.newInstance());
+
+
+        addSlide(AppIntroFragment.newInstance(
+                "Clean App Intros",
+                "This library offers developers the ability to add clean app intros at the start of their apps.",
+                R.mipmap.ic_launcher
+        ));
+
+
+        // Fade Transition
+        setTransformer(AppIntroPageTransformerType.Fade.INSTANCE);
+    }
+
+    @Override
+    protected void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        finish();
+    }
+
+    @Override
+    protected void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        finish();
     }
 }
